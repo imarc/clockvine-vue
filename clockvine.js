6d849eaa2fbb61168ce3236dc3013a8e41ea2e06
Kevin Hamer <kevin@imarc.com>
2017-09-19 16:10:50 -0400

    switch clockvine to use babel and a prepublish hook

--- clockvine.js
+++
@@ -1,9 +0,0 @@
-import vueMixin from './src/vueMixin';
-import vuexModule from './src/vuexModule';
-import editableVueMixin from './src/editableVueMixin';
-
-export {
-    editableVueMixin,
-    vueMixin,
-    vuexModule,
-};
---
+++ src/clockvine.js
@@ -0,0 +1,9 @@
+import vueMixin from './vueMixin';
+import vuexModule from './vuexModule';
+import editableVueMixin from './editableVueMixin';
+
+export {
+    editableVueMixin,
+    vueMixin,
+    vuexModule,
+};
