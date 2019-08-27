daa7aaa315a12dcd5c6ee72f284329283461bacb
Kevin Hamer <kevin@imarc.com>
2019-08-27 10:02:03 -0400

    add search component

---
+++ src/Search.vue
@@ -0,0 +1,42 @@
+<template>
+    <input type="search" :value="internalValue" @input="input" @click="$emit('input', value)" :class="{'-isTyping': isTyping}" />
+</template>
+<script>
+import Vue from 'vue';
+import {debounce} from 'lodash';
+
+export default {
+    data: () => ({
+        internalValue: null,
+        isTyping: false,
+    }),
+    methods: {
+        input({target: {value}}) {
+            this.internalValue = value;
+            this.$emit('isTyping', this.isTyping = true);
+            return this.debouncedInput(value);
+        },
+        debouncedInput: debounce(function(value) {
+            this.$emit('isTyping', this.isTyping = false);
+            return this.$emit('input', value);
+        }, 300),
+    },
+    props: {
+        value: {
+            type: String,
+            required: true,
+        },
+    },
+    mounted() {
+        console.log('mounted', this.value);
+        this.internalValue = this.value;
+    },
+
+    watch: {
+        value(value) {
+            console.log('watch value', value);
+            this.internalValue = value;
+        },
+    },
+}
+</script>
