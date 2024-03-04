b3dc93bcbb1e79dc10638e6f47c7d6a236e6c29b
Kevin Hamer <kevin@imarc.com>
2024-03-04 16:40:38 -0500

    remove outdated

--- src/JsonSingletonApi.js
+++
@@ -1,8 +0,0 @@
-import JsonApi from './JsonApi.js'
-
-export default function JsonSingletonApi (baseUrl, options) {
-  return new JsonApi(baseUrl, {
-    ...options,
-    FormatOptions: { format: (_, url) => url }
-  })
-}
